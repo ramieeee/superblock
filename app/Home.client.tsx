1ef577c3688e8f247680794c65f0b4ba1663cb95
ramieeee <rambotty@gmail.com>
2024-06-27 21:37:41 +0900

    Fix Square component to be rendered with ballon. Add LICENSE file and add balloon svg

--- app/Home.client.tsx
+++ app/Home.client.tsx
@@ -3,17 +3,45 @@
 import { useState, useEffect } from "react";
 import styles from "./HomeClient.module.scss";
 
+// components
+import Square from "@/components/Square/Square";
+
 export default function HomeClient() {
   const M = 6;
 
+  // 60% 확률로 1이 나오도록 초기화
   const [squares, setSquares] = useState(
-    Array.from({ length: M * M }, () => "")
+    Array.from({ length: M * M }, () => {
+      return {
+        value: Math.random() > 0.6 ? 1 : 0, // 풍선이 있으면 1, 없으면 0
+        connectionCnt: 0, // 연결된 풍선의 개수
+      };
+    })
   );
+
+  useEffect(() => {
+    console.log(squares);
+    // const ones = squares.filter((x) => x === 1).length;
+    // const zeros = squares.filter((x) => x === 0).length;
+    // console.log(ones, zeros);
+  }, []);
 
   return (
     <div className={styles.HomeClient}>
-      <h1>Home</h1>
-      <p>Welcome to the home page.</p>
+      <div className={styles.squareBox}>
+        {squares.map((square, idx) => {
+          return (
+            <>
+              <Square
+                key={idx}
+                value={square.value}
+                connectionCnt={square.connectionCnt}
+                onClick={() => console.log("click")}
+              />
+            </>
+          );
+        })}
+      </div>
     </div>
   );
 }
--- components/Square/Square.tsx
+++ components/Square/Square.tsx
@@ -1,7 +1,32 @@
-export default function Square() {
+import styles from "./Square.module.scss";
+import Image from "next/image";
+import Balloon from "@/assets/balloon.svg";
+
+type SquareProps = {
+  value: number;
+  connectionCnt: number;
+  onClick: () => void;
+};
+
+export default function Square({ value, connectionCnt, onClick }: SquareProps) {
   return (
-    <>
-      <>square</>
-    </>
+    <div
+      className={styles.Square}
+      style={{
+        width: "30px",
+        height: "30px",
+        backgroundColor: "#ffffff",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        cursor: "pointer",
+        border: "1px solid #000",
+      }}
+      onClick={onClick}
+    >
+      {value === 1 ? (
+        <Image src={Balloon} alt="Balloon" width={20} height={20} />
+      ) : null}
+    </div>
   );
 }
