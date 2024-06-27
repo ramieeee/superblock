b58e6c7034032f564e9e04acff3468c36c4c5dee
ramieeee <rambotty@gmail.com>
2024-06-27 20:47:07 +0900

    Add square component

--- app/Home.client.tsx
+++ app/Home.client.tsx
@@ -1,6 +1,15 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import styles from "./HomeClient.module.scss";
 
 export default function HomeClient() {
+  const M = 6;
+
+  const [squares, setSquares] = useState(
+    Array.from({ length: M * M }, () => "")
+  );
+
   return (
     <div className={styles.HomeClient}>
       <h1>Home</h1>
