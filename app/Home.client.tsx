458f937a1e6127f3566e5307b4d713d4c203c398
ramieeee <rambotty@gmail.com>
2024-06-27 20:42:57 +0900

    Add HomeClient Page

---
+++ app/Home.client.tsx
@@ -0,0 +1,10 @@
+import styles from "./HomeClient.module.scss";
+
+export default function HomeClient() {
+  return (
+    <div className={styles.HomeClient}>
+      <h1>Home</h1>
+      <p>Welcome to the home page.</p>
+    </div>
+  );
+}
